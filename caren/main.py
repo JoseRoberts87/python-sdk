7902068d9e431ff59a524d5fb56147aaf9903896
JoseRoberts87 <robertslafamilia@outlook.com>
2017-05-10 01:02:54 -0400

    added main project

---
+++ caren/main.py
@@ -0,0 +1,4 @@
+import speech_to_text_v1, text_to_speech_v1, speech_capture
+import play_audio
+
+play_audio()
--- examples/play_audio.py
+++
@@ -1,4 +0,0 @@
-from os.path import join, dirname
-import os
-
-os.system(join(dirname(__file__), '../resources/output2.wav'))
